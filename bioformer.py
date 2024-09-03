f4d55112b192cc6f669eea8375f06b7e8d1476c6
Giacomo Ciro <giacomociro02@gmail.com>
2024-09-03 12:57:10 +0200

    init row wise attn

--- bioformer.py
+++ bioformer.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import math
 
 import torch 
 import torch.nn as nn
@@ -78,7 +79,7 @@ class OuterProductMean(nn.Module):
         return outer
 
 
-class RowAttentionWithPairBias:
+class RowAttentionWithPairBias(nn.Module):
     """
     Input:
         RNA:
@@ -93,6 +94,117 @@ class RowAttentionWithPairBias:
 
     Compute attn between any two vectors i,j in the input RNA using TF[i,j] as pair bias in the attn computation
     """
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        c_in,
+        c_hidden,
+        no_heads,
+        pair_bias=False,
+        c_z=None,
+        gating=True,
+    )-> None:
+        """
+        Args:
+            c_in:
+                Input channel dimension
+            c_hidden:
+                Per-head hidden channel dimension
+            no_heads:
+                Number of attention heads
+            pair_bias:
+                Whether to use pair embedding bias
+            c_z:
+                Pair embedding channel dimension. Ignored unless pair_bias
+                is true
+            gating:
+                Whether to use gated attention or not
+        """
+        super(RowAttentionWithPairBias, self).__init__()
 
+        self.c_in = c_in
+        self.c_hidden = c_hidden
+        self.no_heads = no_heads
+        self.pair_bias = pair_bias
+        self.c_z = c_z
+        self.gating = gating
+
+        self.layer_norm_m = nn.LayerNorm(self.c_in)
+
+        self.layer_norm_z = None
+        self.linear_z = None
+        if self.pair_bias:
+            self.layer_norm_z = nn.LayerNorm(self.c_z)
+            self.linear_z = nn.Linear(self.c_z, self.no_heads, bias=False)
+
+        self.linear_q = nn.Linear(self.c_q, self.c_hidden * self.no_heads, bias=False)
+        self.linear_k = nn.Linear(self.c_k, self.c_hidden * self.no_heads, bias=False)
+        self.linear_v = nn.Linear(self.c_v, self.c_hidden * self.no_heads, bias=False)
+        self.linear_o = nn.Linear(self.c_hidden * self.no_heads, self.c_q)
+
+        self.linear_g = None
+        if self.gating:
+            self.linear_g = nn.Linear(self.c_q, self.c_hidden * self.no_heads, init="gating")
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self,
+            m: torch.Tensor,
+            z: Optional[torch.Tensor] = None,
+    )->torch.Tensor:
+        """
+        Args:
+            m:
+                [*, N_res, C_m] MSA embedding
+            z:
+                [*, N_res, N_res, C_z] pair embedding. Required only if
+                pair_bias is True
+        """
+        m = self.layer_norm_m(m)
+
+        q = self.linear_q(m)
+        k = self.linear_k(m)
+        v = self.linear_v(m)
+
+        # [*, Q/K, H, C_hidden]
+        q = q.view(q.shape[:-1] + (self.no_heads, -1))
+        k = k.view(k.shape[:-1] + (self.no_heads, -1))
+        v = v.view(v.shape[:-1] + (self.no_heads, -1))
+
+        # TODO: check dimensions here, is this line needed?
+        # [*, H, Q/K, C_hidden]
+        # q = q.transpose(-2, -3)
+        # k = k.transpose(-2, -3)
+        # v = v.transpose(-2, -3)
+
+        q /= math.sqrt(self.c_hidden)
+
+        # TODO: check dimensions here, is this line needed?
+        # [*, H, C_hidden, K]
+        # key = permute_final_dims(k, (1, 0))
+
+        # [*, H, Q, K]
+        a = torch.matmul(q, k)
+
+        if self.pair_bias:
+            z = self.layer_norm_z(z)
+            z = self.linear_z(z)
+            # TODO: sum to attn score
+
+        a = nn.Functional.softmax(a, -1)
+
+        # [*, H, Q, C_hidden]
+        a = torch.matmul(a, v)
+        
+        if self.gating:
+            g = self.sigmoid(self.linear_g(m))
+        
+            # [*, Q, H, C_hidden]
+            g = g.view(g.shape[:-1] + (self.no_heads, -1))
+            o = o * g
+
+        # [*, Q, H * C_hidden]
+        o = o.reshape(o.shape[:-2] + (-1,))     # flatten H dim
+
+        # [*, Q, C_q]
+        o = self.linear_o(o)
+        return a
